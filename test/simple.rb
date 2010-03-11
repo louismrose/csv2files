553a6c952c79fe7c2712f1e237e307dfced2232d
Louis Rose <louismrose@gmail.com>
2010-03-11 18:01:02 +0000

    Added further assertions to simple test.

--- test/simple.rb
+++ test/simple.rb
@@ -1,17 +1,19 @@
 require 'test/unit'
 require 'fileutils'
 
+require 'csv_to_files.rb'
+
 class Simple < Test::Unit::TestCase
   
-  @@output_path = 'simple'
+  @@test_path   = 'simple'
+  @@output_path = 'simple/output'
   
   def setup
-    FileUtils.rm_rf(@@output_path) if File.directory?(@@output_path)
+    FileUtils.rm_rf(@@test_path) if File.directory?(@@test_path)
+    Dir.mkdir(@@test_path)
     Dir.mkdir(@@output_path)
     
-    csv = "Exam Number,Report,Code
-louis,10,5
-dimitar,10,10"
+    csv = "Exam Number,Report,Code\nlouis,10,5\ndimitar,10,10"
     
     File.open('simple/input.csv', 'w') {|f| f.write(csv) }
     
@@ -19,11 +21,24 @@ dimitar,10,10"
   end
   
   def teardown
-    FileUtils.rm_rf(@@output_path) if File.directory?(@@output_path)
+    FileUtils.rm_rf(@@test_path) if File.directory?(@@test_path)
   end
   
   def test_that_two_files_are_generated
-    assert_equal 2, Dir.entries(@@output_path) - ['.', '..']
+    assert_equal 2, Simple.output_filenames.size
+  end
+  
+  def test_that_files_with_correct_names_are_generated
+    assert_equal ['dimitar', 'louis'], Simple.output_filenames.sort
   end
 
+  def test_that_files_have_correct_contents
+    assert_equal "Report=10\nCode=5\n",  File.open("#{@@output_path}/louis",   "r").read
+    assert_equal "Report=10\nCode=10\n", File.open("#{@@output_path}/dimitar", "r").read
+  end
+
+private
+  def self.output_filenames
+    Dir.entries(@@output_path) - ['.', '..']
+  end
 end
